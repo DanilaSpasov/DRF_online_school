c1a2bc69392b927bba58c5060630c6343fab27ef
Danila Spasov <spasov2000@mail.ru>
2026-08-10 16:57:40 +0300

    Add course update timestamp

--- lms/models.py
+++ lms/models.py
@@ -14,6 +14,7 @@ class Course(models.Model):
         verbose_name="Владелец",
     )
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
 
     def __str__(self):
         return self.title
